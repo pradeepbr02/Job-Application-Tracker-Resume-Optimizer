a6dc14b11238aff6ff56cfb058ec7f4041bf4062
pradeepbr02 <pradeepbr177@gmail.com>
2026-02-22 21:00:52 +0530

    added docker files and compose files

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const startServer = async()=>{
         await mongoose.connect(process.env.MONGO_URI) 
         console.log("Connected to DB")
         
-        app.listen(PORT , ()=>{
+        app.listen(PORT , "0.0.0.0" , ()=>{
             console.log(`Server is running on port ${PORT}`)
         })
     }
